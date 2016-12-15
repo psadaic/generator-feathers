ab9f03164b117bbd8d7d68a7a8512d2fc816a9e7
Patrik Sadaić <patriksadaic@gmail.com>
2016-12-15 14:21:16 +0100

    update thinky model template

--- generators/model/templates/thinky.js
+++ generators/model/templates/thinky.js
@@ -10,7 +10,12 @@ const type = thinky.type;
 const r = thinky.r;
 
 const <%= name %>Model = thinky.createModel('<%= name %>', {
-  text: {_type: type.string(), enforce_missing: true},
+  <% if(name === 'user') { %><% for (var i = 0; i < providers.length; i++) { %>
+  <% if (providers[i] === 'local') { %>email: {_type: type.string(), enforce_missing: true},
+  password: {_type: type.string(), enforce_missing: true},
+  <% } else { %><%= providers[i].name %>Id: {_type: type.string()},
+  <%= providers[i].name %>: {_type: type.string(), enforce_type: "none"},<% }%><% } %><% } else { %>
+  text: {_type: type.string(), enforce_missing: true},<% } %>
   createdAt: type.date().default(Date.now),
   updatedAt: type.date().default(Date.now)
 });
